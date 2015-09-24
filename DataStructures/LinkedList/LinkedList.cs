0524d0a48abf66c42fa2e128e03269c5d788a4b5
Sean Veale <wilcravenmckrye@gmail.com>
2015-09-23 22:34:38 -0400

    Implementation of a generic linked list with simple console app test

--- DataStructures/LinkedList/LinkedList.cs
+++ DataStructures/LinkedList/LinkedList.cs
@@ -16,27 +16,142 @@ namespace DataStructures.LinkedList
     public class LinkedList<T>
     {
         private LinkedListNode<T> head = null; // List is intially empty.
-
+        private LinkedListNode<T> current = null; // Curret node, used to 
+        private int size = 0; 
+        public int Count
+        {
+            get { return size; }
+        }
         // Add a node at the start of of the list. 
-        public void AddNode (T t)
+        public void AddAtStart(T t)
         {
             LinkedListNode<T> newNode = new LinkedListNode<T>();
             newNode.Next = head;
             newNode.Data = t;
             head = newNode;
+            size++;
                
+        }
+
+        public void Add(T t)
+        {
+            size++;
+
+            var node = new LinkedListNode<T>()
+            {
+                Data = t
+            };
+
+            if (head == null)
+            {
+                // this is the first node, make it the head
+                head = node;
+            }
+            else
+            {
+                // This is not the head, Make it curret's next node
+                current.Next = node;
+
+            }
+
+
+            //Makes the ewly added node the current node
+            current = node;  
         }
 
         //Get the last Node in a List
         T GetLast()
         {
             T temp = default(T); // Return the default of the type if list is empty.
+            LinkedListNode<T> current = head;
 
+            while (current != null)
+            {
+                temp = current.Data;
+                current = current.Next;
+            }
             return temp;
         }
 
-        // Add a node After Another Node
+        // List of the nodes, for testing purposes
+        public void ListNodes()
+        {
+            LinkedListNode<T> tempNode = head;
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.Data);
+                tempNode = tempNode.Next;
+            }
+        }
 
+        // Fetch a node at a specifc position 
+        public LinkedListNode<T> Retrieve(int Position)
+        {
+            LinkedListNode<T> tempNode = head;
+            LinkedListNode<T> retNode = null;
+            int count = 0;
+
+            while (tempNode != null)
+            {
+                if (count == Position - 1)
+                {
+                    retNode = tempNode;
+                    break;
+                }                      
+                count++;
+                tempNode = tempNode.Next; 
+
+            }
+
+            return retNode; 
+        }
+
+        //Delete a Node at a specific position 
+        public bool Delete(int Position)
+        {
+            if (Position == 1)
+            {
+                if (Position == size)
+                {
+                    head = null;
+                    current = null;
+                    return true;
+                }
+                else if (Position > size)
+                {
+                    return false;
+                }
+                else
+                {
+                    head = head.Next;
+                    return true;
+                }
+
+            }
+
+            if (Position > 1 && Position <= size)
+            {
+                var tempNode = head;
+                LinkedListNode<T> lastNode = null;
+                int count = 0;
+
+                while (tempNode != null)
+                {   
+                    if (count == Position - 1)
+                    {
+                        lastNode.Next = tempNode.Next;
+                        return true;
+                    }
+                    count++;
+
+                    lastNode = tempNode;
+                    tempNode = tempNode.Next;
+                }
+
+            }
+
+            return false;
+        }
         // Reverse a Linked List.  
 
         // Detect for a circular linked list 
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataStructures.LinkedList;
 
 namespace SvMicroSolutions.DataStructures
 {
@@ -10,6 +10,57 @@ namespace SvMicroSolutions.DataStructures
     {
         static void Main(string[] args)
         {
+           LinkedList<string> list = new LinkedList<string>();
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Add("D");
+            list.Add("E");
+            list.Add("F");
+            list.Add("G");
+            list.Add("H");
+
+            list.ListNodes();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 8");
+            list.Delete(8);
+            list.ListNodes();
+
+            Console.WriteLine();
+            Console.WriteLine("Position 5: " + list.Retrieve(5).Data);
+
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 5");
+            list.Delete(5);
+
+            Console.WriteLine();
+            Console.WriteLine("Position 5: " + list.Retrieve(5).Data);
+
+            Console.WriteLine();
+            list.ListNodes();
+
+            Console.WriteLine("Deleting node 9");
+            list.Delete(9);
+            Console.WriteLine();
+            list.ListNodes();
+
+            Console.WriteLine("Deleting node 1");
+            list.Delete(1);
+            Console.WriteLine();
+            list.ListNodes();
+
+            Console.WriteLine("Deleting node 1");
+            list.Delete(1);
+            Console.WriteLine();
+            list.ListNodes();
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
